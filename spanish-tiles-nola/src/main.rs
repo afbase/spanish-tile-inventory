d7fba0c2fb5fd552b5070b9ac3e59a47678a2519
clinton bowen <clinton.bowen@gmail.com>
2024-08-10 22:20:39 -0700

    updates

--- spanish-tiles-nola/src/main.rs
+++ spanish-tiles-nola/src/main.rs
@@ -4,6 +4,7 @@ use web_sys::{HtmlElement, wasm_bindgen::JsCast};
 use yew::prelude::*;
 use chrono::{DateTime, Utc};
 use log::{Level, Metadata, Record};
+extern crate console_error_panic_hook;
 pub struct MyLogger;
 pub static MY_LOGGER: MyLogger = MyLogger;
 
@@ -67,24 +68,58 @@ impl Component for App {
 }
 
 fn main() {
-    wasm_logger::init(wasm_logger::Config::default());
+    console_error_panic_hook::set_once();
+    // wasm_logger::init(wasm_logger::Config::default());
     log::set_logger(&MY_LOGGER).unwrap();
     log::set_max_level(LevelFilter::Info);
+    info!("{}", "start!");
     web_sys::window()
         .and_then(|window| window.document())
-        .and_then(|document| document.get_element_by_id(DIV_BLOG_NAME))
         .map_or_else(
             || {
-                let log_str = format!(
-                    "Unable to find div {DIV_BLOG_NAME}. Failed to load wasm module successfully."
-                );
-                info!("{}", log_str);
+                let log_str = "failed to load wasm module successfully part 1";
+                let log_string = String::from(log_str);
+                info!("{}", log_string);
                 panic!("{}", log_str);
             },
-            |element| {
-                let div_element = element.dyn_into::<HtmlElement>().unwrap();
-                let renderer = yew::Renderer::<App>::with_root(div_element.into());
-                renderer.render();
+            |document| match document.get_element_by_id(DIV_BLOG_NAME) {
+                Some(div_element) => {
+                    info!("{}", "here we go!");
+                    let renderer = yew::Renderer::<App>::with_root(div_element);
+                    info!("{}", "rendering");
+                    renderer.render();
+                    info!("{}", "rendered");
+                }
+                None => {
+                    let log_str = format!(
+                        "Unable to find div {DIV_BLOG_NAME}. failed to load wasm module successfully part 2"
+                    );
+                    info!("{}", log_str);
+                    panic!("{}", log_str);
+                }
             },
         );
+    // log::set_logger(&MY_LOGGER).unwrap();
+    // log::set_max_level(LevelFilter::Info);
+    // info!("{}", "start!");
+    // web_sys::window()
+    //     .and_then(|window| window.document())
+    //     .and_then(|document| document.get_element_by_id(DIV_BLOG_NAME))
+    //     .map_or_else(
+    //         || {
+    //             let log_str = format!(
+    //                 "Unable to find div {DIV_BLOG_NAME}. Failed to load wasm module successfully."
+    //             );
+    //             info!("{}", log_str);
+    //             panic!("{}", log_str);
+    //         },
+    //         |element| {
+    //             
+    //             let div_element = element.dyn_into::<HtmlElement>().unwrap();
+    //             info!("{}", "unwrapped");
+    //             let renderer = yew::Renderer::<App>::with_root(div_element.into());
+    //             info!("{}", "rendering");
+    //             renderer.render();
+    //         },
+    //     );
 }
